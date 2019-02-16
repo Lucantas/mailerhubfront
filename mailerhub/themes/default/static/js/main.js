9199298b74c7d98bb8d4a064adc24f66de4536b2
Lucas <lucas.lucantas38@gmail.com>
2019-02-16 16:19:10 -0200

    Created submenus on the sidebar menu, created some stub content to test the requests

--- mailerhub/themes/default/static/js/main.js
+++ mailerhub/themes/default/static/js/main.js
@@ -6,20 +6,6 @@ sidebarToggle.onclick = function(){
     s.show();
 }
 
-s.children.forEach((link) => {
-    link.onclick = function(e){
-        e.preventDefault();
-        
-        fetch(link.href)
-        .then((res) => { return res.text() })
-        .then((res) => {
-            const content = res;
-            mainContent.innerHTML = content;
-            if (isMobile) s.hide();
-        });
-    }
-})
-
 let isMobile = screen.width >= 1024 ? false : true;
 
 
--- mailerhub/themes/default/static/js/sidebar.js
+++ mailerhub/themes/default/static/js/sidebar.js
@@ -7,10 +7,57 @@ const SideBar = function(elm) {
     this.children = [];
     this.getChildren = () =>{
         this.children = [];
-        this.elm.querySelectorAll("li>a").forEach((item) => {
+        this.elm.querySelectorAll("ul>li>a").forEach((item) => {
             this.children.push(item);
         })
+        this.handleLinks();
     }
+
+    this.handleLinks = () =>{
+        this.children.forEach((link) => {
+            link.onclick = function(e){
+                e.preventDefault();
+                const parent = this.parentNode;
+
+                if (parent.classList.contains("has-children")){
+                    const sublist = parent.querySelector("ul");
+                    const arrow = parent.querySelector("i.arrow");
+                    if (sublist.offsetHeight > 0){
+                        sublist.style.height = 0;
+                        arrow.style.animation = "fa-spin 1s 2 linear reverse"
+                        setTimeout(() => {
+                            arrow.classList.add("fa-chevron-down");
+                            arrow.classList.remove("fa-chevron-left");
+                            arrow.style.animation = "";
+                        },299)
+                    } else {
+                        sublist.style.height = sublist.childrenHeight() + "px";
+                        arrow.style.animation = "fa-spin 1s 2 linear"
+                        setTimeout(() => {
+                            arrow.classList.add("fa-chevron-left");
+                            arrow.classList.remove("fa-chevron-down");
+                            arrow.style.animation = "";
+                        },299)
+                    }
+                } else {
+                    fetch(link.href)
+                    .then((res) => { return res.text() })
+                    .then((res) => {
+                        const content = res;
+                        mainContent.innerHTML = content;
+                        const curActive = link.closest("ul.menu").querySelector("li.active");
+                        if (curActive != undefined && curActive != elm){
+                            curActive.classList.remove("active")              
+                        }
+                        link.parentNode.classList.add("active");      
+                        if (isMobile) s.hide();
+                    });
+
+                }                
+            }        
+        }) 
+    }
+    
     this.hide = () =>{
         this.elm.style.marginLeft = this.defaultStyle.marginLeft;
         content.classList.remove("sidebar-open");
